c69254e820666e528120f8d18c7796c37c3c64d5
Cai Chen <cac005@ucsd.edu>
2019-08-06 11:21:32 -0700

    Initial Setup Of The Project

---
+++ src/main/java/caseychen/springboot/springbootpetclinic/SpringbootPetclinicApplication.java
@@ -0,0 +1,13 @@
+package caseychen.springboot.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetclinicApplication.class, args);
+    }
+
+}
