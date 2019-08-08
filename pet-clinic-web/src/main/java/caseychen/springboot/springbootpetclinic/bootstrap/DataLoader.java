f1e54c011c45a23715ad394e8339c9e88018485b
Cai Chen <cac005@ucsd.edu>
2019-08-08 15:45:18 -0700

    Refactoring Map Implementation to abstract ID generation, Closes #23

--- pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Flona");
         owner2.setLastName("Glenanna");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owenrs...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Zai");
         vet2.setLastName("Gou");
 
