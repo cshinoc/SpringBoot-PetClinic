e0c953ea0955a94d08642158b737613794a02ab5
Cai Chen <cac005@ucsd.edu>
2019-08-08 15:33:53 -0700

    Adding Vets List and updates controllers, Closes #15

--- pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/bootstrap/DataLoader.java
@@ -47,7 +47,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("Zai");
         vet2.setLastName("Gou");
 
--- pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package caseychen.springboot.springbootpetclinic.controllers;
 
 
+import caseychen.springboot.springbootpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
