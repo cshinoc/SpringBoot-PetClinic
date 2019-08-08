cfdaa008404ffc18a0495548d4312f3e40156e0e
Cai Chen <cac005@ucsd.edu>
2019-08-08 13:52:12 -0700

    Implement Map based Service, Closes #6

---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package caseychen.springboot.springbootpetclinic.services.map;
+
+import caseychen.springboot.springbootpetclinic.model.Pet;
+import caseychen.springboot.springbootpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
+
---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package caseychen.springboot.springbootpetclinic.services.map;
+
+import caseychen.springboot.springbootpetclinic.model.Vet;
+import caseychen.springboot.springbootpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
