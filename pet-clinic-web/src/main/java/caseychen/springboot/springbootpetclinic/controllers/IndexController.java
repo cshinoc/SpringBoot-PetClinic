c65499afe7475c28a29d240d36ab2593ef8361a9
Cai Chen <cac005@ucsd.edu>
2019-08-08 14:06:54 -0700

    Adding Index Page and Controller, Closes #8

---
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package caseychen.springboot.springbootpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
