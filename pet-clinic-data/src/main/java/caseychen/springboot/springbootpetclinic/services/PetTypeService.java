8c2f87234d9ec6b4419dbff5da7a0ba4b3cf240a
Cai Chen <cac005@ucsd.edu>
2019-08-09 11:33:22 -0700

    Adding PetTypeService, Closes #29

---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package caseychen.springboot.springbootpetclinic.services;
+
+import caseychen.springboot.springbootpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
