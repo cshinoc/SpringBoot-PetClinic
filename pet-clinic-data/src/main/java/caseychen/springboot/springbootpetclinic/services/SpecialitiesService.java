62cb063d516bb981122c70b3ef8cb5fe3557cb74
Cai Chen <cac005@ucsd.edu>
2019-08-09 14:36:16 -0700

    Adding Specicalties Map Implementation, Closes #31

---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package caseychen.springboot.springbootpetclinic.services;
+
+import caseychen.springboot.springbootpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package caseychen.springboot.springbootpetclinic.services.map;
+
+import caseychen.springboot.springbootpetclinic.model.Speciality;
+import caseychen.springboot.springbootpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
