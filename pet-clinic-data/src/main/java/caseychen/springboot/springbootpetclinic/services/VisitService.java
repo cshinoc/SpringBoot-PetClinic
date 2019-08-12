174cf0469fc61c52479808a19339d292fc038ddf
Cai Chen <cac005@ucsd.edu>
2019-08-11 22:39:41 -0700

    Create Visit Map Service, Closes #46

---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package caseychen.springboot.springbootpetclinic.services;
+
+import caseychen.springboot.springbootpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package caseychen.springboot.springbootpetclinic.services.map;
+
+import caseychen.springboot.springbootpetclinic.model.Visit;
+import caseychen.springboot.springbootpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+         || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
