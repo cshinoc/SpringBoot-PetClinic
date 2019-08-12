46f13870409c53d015e42cb23e2e023cb3ee7437
Cai Chen <cac005@ucsd.edu>
2019-08-12 15:28:42 -0700

    Fixed Owner Display, Closes #53

--- pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package caseychen.springboot.springbootpetclinic.controllers;
 import caseychen.springboot.springbootpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -29,4 +32,12 @@ public class OwnerController {
 
         return "notImplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 }
--- pet-clinic-web/src/test/java/caseychen/springboot/springbootpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/caseychen/springboot/springbootpetclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -61,6 +62,16 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
                 .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 
     @Test
