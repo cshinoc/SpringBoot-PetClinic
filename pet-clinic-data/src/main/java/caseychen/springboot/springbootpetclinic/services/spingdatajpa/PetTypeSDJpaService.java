fd6f09cfa9532e039f6b3fc7ac73d3c5531af38e
Cai Chen <cac005@ucsd.edu>
2019-08-11 22:06:44 -0700

    Create Pet Type Spring Data JPA, Closes #45

---
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/spingdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,51 @@
+package caseychen.springboot.springbootpetclinic.services.spingdatajpa;
+
+import caseychen.springboot.springbootpetclinic.model.PetType;
+import caseychen.springboot.springbootpetclinic.repositories.PetTypeRepository;
+import caseychen.springboot.springbootpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petTypes = new HashSet<>();
+
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
