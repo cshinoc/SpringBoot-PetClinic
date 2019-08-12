619bb44e7fd271a2551138392b13df60e3111d89
Cai Chen <cac005@ucsd.edu>
2019-08-11 22:01:24 -0700

    Create Vets Spring Data JPA, Closes #41

--- pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/spingdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/caseychen/springboot/springbootpetclinic/services/spingdatajpa/OwnerSDJpaService.java
@@ -13,14 +13,14 @@ import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
-public class OwnerSDJpa implements OwnerService {
+public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
     private final PetRepository petRepository;
     private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJpa(OwnerRepository ownerRepository, PetRepository petRepository,
-                      PetTypeRepository petTypeRepository) {
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
         this.petRepository = petRepository;
         this.petTypeRepository = petTypeRepository;
