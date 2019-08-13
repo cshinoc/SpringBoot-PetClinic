17d53bd9bb06b390eb961db9a1606f6d9e1acb1e
Cai Chen <cac005@ucsd.edu>
2019-08-13 15:09:22 -0700

    Implement Vets List View, Closes #65

--- pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/caseychen/springboot/springbootpetclinic/controllers/VetController.java
@@ -1,24 +1,35 @@
 package caseychen.springboot.springbootpetclinic.controllers;
 
 
+import caseychen.springboot.springbootpetclinic.model.Vet;
 import caseychen.springboot.springbootpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
-    private final VetService vetService;
+    VetService vetService;
 
     public VetController(VetService vetService) {
         this.vetService = vetService;
     }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(Model model){
-        model.addAttribute("vets", vetService.findAll());
+    public String listVets(Model model) {
 
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 }
+
